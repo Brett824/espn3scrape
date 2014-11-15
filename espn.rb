623ecf389e55c632df237aae70a9ba405faaffb4
Brett824 <bsheingo@u.rochester.edu>
2014-11-14 22:34:18 -0500

    added willow support
    grabs fixtures from willow, adds extra rows for test matches on willow,
    merges with espn and sorts. added willow/espn column to table.

--- espn.rb
+++ espn.rb
@@ -4,8 +4,9 @@ require 'open-uri'
 require 'net/http'
 require 'date'
 require 'json'
+require 'active_support/time'
 
-broadcasts = []
+broadcasts_espn = []
 
 month = "%02d" % Time.now.month #start from the current month
 
@@ -21,13 +22,54 @@ month = "%02d" % Time.now.month #start from the current month
 		d = Date.strptime("#{e.children[0].text.strip}/#{Time.now.year}", "%m/%d/%Y").strftime("%Y-%m-%d")
 		puts "#{d} #{e.children[1].text} #{e.children[3].text[0..-34]}"
 		b = {}
+		b["site"] = "espn.ico"
+		b["name"] = e.children[3].text[0..-34].strip
 		b["date"] = d
 		b["time"] = e.children[1].text
-		b["name"] = e.children[3].text[0..-34]
-		broadcasts.push(b)
+		broadcasts_espn.push(b)
 	end
 
 end
 
+broadcasts_willow = []
+
+url = "http://willowfeeds.willow.tv/fixtureByDate.json"
+response = open(url).read
+
+
+obj = JSON.parse(/HandleFixturesByDate\((.*)\)/.match(response)[1])
+
+obj["matchDetails"].each do |match|
+	if !match["smname"].downcase.include?("test")
+		b = {}
+		date = DateTime.parse(match["st"])
+		b["site"] = "willow.ico"		
+		b["name"] =  "#{match["t1"]} vs. #{match["t2"]} (#{match["smname"]})"
+		b["date"] = date.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d")
+		b["time"] = date.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M %p EST").strip
+		# p b
+		puts "#{b["date"]} #{b["time"]} #{b["name"]}"
+		broadcasts_willow.push(b)
+	else
+		first_date = DateTime.parse(match["st"])
+		for i in 1..5
+			b = {}
+			date = first_date + (i-1)
+			b["site"] = "willow.ico"
+			b["name"] = "#{match["t1"]} vs. #{match["t2"]} (#{match["smname"]}, Day #{i})"
+			b["date"] = date.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d")
+			b["time"] = date.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M %p EST").strip
+			# p b
+			puts "#{b["date"]} #{b["time"]} #{b["name"]}"
+			broadcasts_willow.push(b)
+		end
+	end
+end
+
+broadcasts = (broadcasts_espn + broadcasts_willow).sort do |a,b| 
+	t_a = DateTime.parse("#{a["date"]}T#{a["time"]}")
+	t_b = DateTime.parse("#{b["date"]}T#{b["time"]}")
+	t_a <=> t_b
+end
 
 File.open("#{File.dirname(__FILE__)}/broadcasts.json", 'w') {|f| f.write(broadcasts.to_json) }
