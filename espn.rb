6e4c9e975af7734bc14281959e1deb3133663ea8
Brett824 <bsheingo@u.rochester.edu>
2014-12-12 21:11:04 -0500

    removed hard coded 2014 year
    also better dates in general
    
    changed text node handling too

--- espn.rb
+++ espn.rb
@@ -6,24 +6,30 @@ require 'date'
 require 'json'
 require 'active_support/time'
 
+puts "Starting fixture loading"
+
 broadcasts_espn = []
 
+puts "Loading ESPN"
+
 month = "%02d" % Time.now.month #start from the current month
+year = Time.now.year
+day = Time.now.day
 
-#check 99 days from the start of the month
-(1..99).each do |i|
+#check from today to day "99" of the month
+(day..99).each do |i|
 
-	url = "http://espn.go.com/watchespn/index?tabType=upcoming&xhr=1&startDate=2014#{month}#{i}&searchTerm="
+	url = "http://espn.go.com/watchespn/index?tabType=upcoming&xhr=1&startDate=#{year}#{month}#{i}&searchTerm="
 	doc = Nokogiri::HTML(open(url))
 
 	events = doc.css("section#CR").css("ul.league-obj").css("li") #could make this 1 css selector, clearer to chain them imo
 
 	events.each do |e|
-		d = Date.strptime("#{e.children[0].text.strip}/#{Time.now.year}", "%m/%d/%Y").strftime("%Y-%m-%d")
-		puts "#{d} #{e.children[1].text} #{e.children[3].text[0..-34]}"
+		d = Date.strptime("#{e.children[0].text.strip}/#{(Date.today + i - day).year}", "%m/%d/%Y").strftime("%Y-%m-%d")
+		puts "#{d} #{e.children[1].text} #{e.children[3].xpath('text()')}"
 		b = {}
 		b["site"] = "espn.ico"
-		b["name"] = e.children[3].text[0..-34].strip
+		b["name"] = e.children[3].xpath('text()')
 		b["date"] = d
 		b["time"] = e.children[1].text
 		broadcasts_espn.push(b)
@@ -32,6 +38,8 @@ month = "%02d" % Time.now.month #start from the current month
 end
 
 broadcasts_willow = []
+
+puts "Loading Willow"
 
 url = "http://willowfeeds.willow.tv/fixtureByDate.json"
 response = open(url).read
@@ -66,6 +74,8 @@ obj["matchDetails"].each do |match|
 	end
 end
 
+puts "Saving to JSON"
+
 broadcasts = (broadcasts_espn + broadcasts_willow).sort do |a,b| 
 	t_a = DateTime.parse("#{a["date"]}T#{a["time"]}")
 	t_b = DateTime.parse("#{b["date"]}T#{b["time"]}")
@@ -73,3 +83,5 @@ broadcasts = (broadcasts_espn + broadcasts_willow).sort do |a,b|
 end
 
 File.open("#{File.dirname(__FILE__)}/broadcasts.json", 'w') {|f| f.write(broadcasts.to_json) }
+
+puts "Done"
